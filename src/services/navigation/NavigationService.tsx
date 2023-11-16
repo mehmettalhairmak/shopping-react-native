d3c002a0fdbc2f1c576d0617f4f214fccbeef2da
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2023-11-16 09:33:46 +0300

    Added custom header

--- src/services/navigation/NavigationService.tsx
+++ src/services/navigation/NavigationService.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabNavigation from './TabNavigation';
 import ProductDetailsScreen from '../../screens/ProductDetailsScreen';
 import { RootStackParams } from './types';
+import CustomHeader from '../../components/CustomHeader';
 
 const Stack = createNativeStackNavigator<RootStackParams>();
 
@@ -16,6 +17,10 @@ const NavigationService = () => {
         <Stack.Screen name="TabNavigation" component={TabNavigation} />
         <Stack.Screen
           name="ProductDetailsScreen"
+          options={{
+            headerShown: true,
+            header: props => <CustomHeader {...props} />,
+          }}
           component={ProductDetailsScreen}
         />
       </Stack.Navigator>
