21fb026c4ee5f6641fa7447d6a0ceeaac249173f
Mehmet Talha Irmak <mehmetirmaakk@gmail.com>
2023-11-16 13:53:32 +0300

    added feature for favorited list storage

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import NavigationService from './src/services/navigation/NavigationService';
-import { store } from './src/services/redux/store';
+import { persistor, store } from './src/services/redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationService />
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationService />
+      </PersistGate>
     </Provider>
   );
 };
